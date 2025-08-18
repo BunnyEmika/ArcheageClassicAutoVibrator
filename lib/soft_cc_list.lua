af3dad8a54ad2d27f6123378cb27e7a8906cd71f
BunnyEmika <emikaasira@gmail.com>
2025-08-18 22:39:51 +0930

    Fix CC activation error

---
+++ lib/soft_cc_list.lua
@@ -0,0 +1,6 @@
+local softCCList = {
+    'Slow', "Alliance Soldier's Pull", 'Dazed', 'Pulled', "Agrax's Snare", 'Earthen Grip',
+    'Dizziness', 'Mana Force', 'Shock', 'Silence', "Red Dragon's Wind Gust", 'Snare'
+}
+
+return softCCList
